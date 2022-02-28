c5111fedbfe0fbd68a30614004e464263dc12d48
Merka34 <videoscreepyloq@gmail.com>
2022-02-28 08:40:36 -0600

    Conexion DB

---
+++ ActividadesComplementariasITESRC/Models/itesrcne_complementariasContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ActividadesComplementariasITESRC.Models
+{
+    public partial class itesrcne_complementariasContext : DbContext
+    {
+        public itesrcne_complementariasContext()
+        {
+        }
+
+        public itesrcne_complementariasContext(DbContextOptions<itesrcne_complementariasContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=204.93.216.11;user=itesrcne_proy22;password=4veSZ42@Eii4SMt;database=itesrcne_complementarias", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.3.29-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasCharSet("utf8");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
