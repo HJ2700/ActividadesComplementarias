62dcd9ba9bd9c835393f6bf53d128aab56f3f59f
Humberto Ramos <ramoshumberto00@gmail.com>
2022-03-01 08:24:46 -0600

    Services en Startup Carpetas basicas

--- ActividadesComplementariasITESRC/Startup.cs
+++ ActividadesComplementariasITESRC/Startup.cs
@@ -16,6 +16,7 @@ namespace ActividadesComplementariasITESRC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,14 +28,12 @@ namespace ActividadesComplementariasITESRC
             }
 
             app.UseRouting();
+            app.UseFileServer();
 
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    endpoints.MapDefaultControllerRoute();
                 });
-            });
         }
     }
 }
